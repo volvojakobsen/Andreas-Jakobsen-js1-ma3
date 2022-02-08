6f56b40d09f29784231a39aa81297b51c5edf2e3
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2022-02-08 20:54:24 +0100

    think i am done

--- js/question-2.js
+++ js/question-2.js
@@ -1,15 +1,20 @@
 const key = "39999066c3344534906d49c321768c0e";
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=39999066c3344534906d49c321768c0e";
-//let results = document.querySelector(".results");
-//let showResults = results.innerHTML; 
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=39999066c3344534906d49c321768c0el";
+let results = document.querySelector(".results");
 
+function displayError(message = "unknown error") {
+    return `<div class="error">${message}</div>`;
+}
 
 async function getAPI() {
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url);
     const jsonResponse = await response.json();
     const final = jsonResponse.results;
     console.log(final)
     let results = document.querySelector(".results");
+    
+    results.innerHTML = "";
      
 
     for(let i = 0; i < final.length; i++) {
@@ -18,6 +23,10 @@ async function getAPI() {
         }
         results.innerHTML += `<div class="games">Game name: ${final[i].name}</div> <div class="games">Rating: ${final[i].rating}</div><div class="games end">Number of tags: ${final[i].tags.length}</div>`;
         
+    }
+    }
+    catch(error) {
+    results.innerHTML = displayError("An error occured when calling the API");
     }
 }
 
